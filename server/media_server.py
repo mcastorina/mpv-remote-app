1d38d73621ca0221933d240ee57a39533ffa34ff
Miccah Castorina <m.castorina93@gmail.com>
2018-12-15 11:19:28 -0600

    Properly send ACK response
    _ack function will now take a boolean and a string indicating success
    and a message. This json message gets HMAC'd and sent to the client,
    followed by the client and action_id getting cleared.

--- server/media_server.py
+++ server/media_server.py
@@ -1,6 +1,7 @@
 import os
 import hmac
 import json
+import time
 import socket
 import logging
 from collections import OrderedDict
@@ -20,6 +21,10 @@ class MediaServer:
         self.state = 'NORMAL'       # state can be NORMAL or REPEAT
         self.history_size = 32      # max number of commands to remember
         self.history = OrderedDict()
+
+        # attributes that change per connection
+        self.client = None
+        self.action_id = None
     # runs the server
     def run(self, daemon=False):
         pass
@@ -39,7 +44,7 @@ class MediaServer:
                 logging.debug("Received: \"%s\"", data)
                 if data == "health":
                     logging.debug("alive")
-                    self._ack(True)
+                    self._ack(True, None)
                     continue
                 # expect {message: "message", hmac: "HMAC"}
                 data = json.loads(data)
@@ -48,14 +53,29 @@ class MediaServer:
                     logging.info("Authentication failed for command: \"%s\"", str(data))
                     continue
                 data = json.loads(data["message"])
+                # set action_id as the time of the request
+                self.action_id = data["time"]
                 logging.info("Received command: \"%s\"", data)
                 return data
             except Exception as e:
                 logging.error("Error parsing command: \"%s\"", data)
                 logging.error(str(e))
     # sends response back to requester
-    def _ack(self, success):
-        self.sock.sendto(str(success).encode(), self.client)
+    def _ack(self, success, response):
+        # check if action in history
+
+        # generate message
+        t = round(time.time() * 1000)
+        msg = json.dumps({"action": self.action_id, "time": t,
+                          "result": success, "message": response})
+        logging.debug("Sending ACK: \"%s\"", msg)
+
+        h = hmac.new((self.password + str(t)).encode(), msg.encode()).hexdigest()
+        self.sock.sendto(json.dumps({"hmac": h, "message": msg}).encode(), self.client)
+
+        # clear client and action_id
+        self.client = None
+        self.action_id = None
     # authenticates data we received
     def _auth(self, data):
         # expect data = {message: "message", hmac: "HMAC"}
