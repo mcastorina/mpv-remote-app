9c9fbbc59fd92d5cec04b8145eb0be2c2126503a
elig0n <eliran.gonen@gmail.com>
2021-07-22 04:14:58 +0300

    Use fullscreen icon for button instead of copy

--- MpvRemote/app/src/main/java/miccah/laptopremote/MainActivity.java
+++ MpvRemote/app/src/main/java/miccah/laptopremote/MainActivity.java
@@ -124,8 +124,8 @@ public class MainActivity extends Activity {
             setDrawables(android.R.drawable.ic_media_pause,
                          android.R.drawable.ic_media_play);
         ((BackgroundImageButton)findViewById(R.id.full_screen)).
-            setDrawables(R.drawable.vector_arrange_below_off,
-                         R.drawable.vector_arrange_below_on);
+            setDrawables(R.drawable.fullscreen,
+                         R.drawable.fullscreen_exit);
     }
 
     @Override
